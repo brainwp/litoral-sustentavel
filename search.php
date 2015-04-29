3bdf65d75094e126d76cfd932778eb06ccf695a3
Bruno Magrini <brmagrini@gmail.com>
2015-04-29 18:24:06 -0300

    menu responsivo

--- search.php
+++ search.php
@@ -8,30 +8,36 @@
  */
 
 get_header(); ?>
+<?php get_template_part('parts/comp-header'); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+		<div class="">
+				<div id="content-page" class="" role="main">
+					<div id="post-0" class="col-md-12 post error404 not-found">
+						<div class="entry-content">
+							<?php if ( have_posts() ) : ?>
+											<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+											<?php
+											/* Run the loop for the search to output the results.
+											 * If you want to overload this in a child theme then include a file
+											 * called loop-search.php and that will be used instead.
+											 */
+											 get_template_part( 'loop', 'search' );
+											?>
+							<?php else : ?>
+											<div id="post-0" class="post no-results not-found">
+												<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
+												<div class="entry-content">
+													<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
+													<?php get_search_form(); ?>
+												</div><!-- .entry-content -->
+											</div><!-- #post-0 -->
+							<?php endif; ?>
+						</div><!-- .entry-content -->
+					</div><!-- #post-0 -->
 
-<?php if ( have_posts() ) : ?>
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				<?php
-				/* Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-search.php and that will be used instead.
-				 */
-				 get_template_part( 'loop', 'search' );
-				?>
-<?php else : ?>
-				<div id="post-0" class="post no-results not-found">
-					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
-					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</div><!-- #post-0 -->
-<?php endif; ?>
-			</div><!-- #content -->
+			</div><!-- #content-page -->
+			<div class="col-md-1"></div>
+
 		</div><!-- #container -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_footer('cat'); ?>
